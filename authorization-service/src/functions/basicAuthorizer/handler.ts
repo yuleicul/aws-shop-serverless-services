569f9a5627c5de654be8704c13383520bb51ba00
Yulei Chen <chenyuleiw@gmail.com>
2022-08-08 17:26:07 +0800

    feat(auth): authorizer handler

--- authorization-service/src/functions/basicAuthorizer/handler.ts
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -1,36 +1,63 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
-import { formatJSONResponse } from '@libs/apiGateway';
-import { ForbiddenException } from '@libs/exception/ForbiddenException';
-import { UnauthorizedException } from '@libs/exception/UnauthorizedException';
 import { middyfy } from '@libs/lambda';
+import { APIGatewayAuthorizerHandler } from 'aws-lambda';
 
-const innerHandler = async (token: string) => {
-  const encodedCreds = token.split(' ')[1]
-  const buff = Buffer.from(encodedCreds, 'base64')
-  const plainCreds = buff.toString('utf-8').split(':')
-  const username = plainCreds[0]
-  const password = plainCreds[1]
-
-  console.log(`username: ${username}, password: ${password}`)
-
-  const storedUserPassword = process.env[username]
-
-  if (!storedUserPassword || (storedUserPassword !== password)) return new ForbiddenException()
-
-  return formatJSONResponse(`Hello, ${username}`)
+const generatePolicy = (principalId, resource, effect = 'Allow') => {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource
+        }
+      ]
+    }
+  }
 }
 
-export const basicAuthorizer: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
-  console.log('=== REQUEST ===', event);
-
-  // if (event['type'] !== 'TOKEN') {
-  if (!event.headers.Authorization) {
-    return new UnauthorizedException()
+const innerHandler = (token: string) => {
+  try {
+    const encodedCreds = token.split(' ')[1]
+    const buff = Buffer.from(encodedCreds, 'base64')
+    const plainCreds = buff.toString('utf-8').split(':')
+    const username = plainCreds[0]
+    const password = plainCreds[1]
+  
+    console.log(`username: ${username}, password: ${password}`)
+  
+    const storedUserPassword = process.env[username]
+  
+    // if (!storedUserPassword || (storedUserPassword !== password)) 
+    // return new ForbiddenException()
+    const effect = !storedUserPassword || storedUserPassword != password ? 'Deny' : 'Allow'
+  
+    // return formatJSONResponse(`Hello, ${username}`)
+    return [encodedCreds, effect]
+  } catch (error) {
+    throw error
   }
+}
 
-  const response = await innerHandler(event.headers.Authorization) 
-  console.log('=== RESPONSE ===', response)
-  return response
+export const basicAuthorizer: APIGatewayAuthorizerHandler = (event, _context, callback) => {
+  try {
+    console.log('=== REQUEST ===', event);
+  
+    if (event.type !== 'TOKEN') {
+      // return new UnauthorizedException()
+      callback('Unauthorized')
+    }
+  
+    // @ts-ignore
+    const [encodedCreds, effect] = innerHandler(event.authorizationToken) 
+    const policy = generatePolicy(encodedCreds, event.methodArn, effect)
+    callback(null, policy)
+
+    console.log('=== POLICY ===', policy)
+  } catch (error) {
+    callback(error)
+  }
 }
 
 export const main = middyfy(basicAuthorizer);
